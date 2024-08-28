2be9cd37cb3320f3003f0182b5267ddf023bb0d2
omarabdul432 <abdomar0748@gmail.com>
2024-08-28 22:41:22 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private firestore: Firestore) { }
+
   title = 'crud';
+  async ngOnInit() {
+    await addDoc(collection(this.firestore, 'users'), {
+      username: 'omar',
+      password: 'omar123'
+    })
+
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    apiKey: "AIzaSyBpKi7FESyr3yu_1N93QCp-ieLVgGOJgkI",
+    authDomain: "angular-aa33a.firebaseapp.com",
+    projectId: "angular-aa33a",
+    storageBucket: "angular-aa33a.appspot.com",
+    messagingSenderId: "411451883384",
+    appId: "1:411451883384:web:bfd131489b78a570e3128d"
+};
